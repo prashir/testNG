a53df847cc18bef5febd2b76e65c74fcea429ade
Prashir Pal <prashirpal@gmail.com>
2024-04-18 00:22:12 +0530

    first commit

---
+++ src/org/testNG/com/Class1.java
@@ -0,0 +1,16 @@
+package org.testNG.com;
+
+import org.testng.annotations.Test;
+
+public class Class1 {
+
+	@Test
+	public void a() {
+		System.out.println("testing");
+	}
+	
+	@Test
+	public void b() {
+		System.out.println("bye");
+	}
+}
